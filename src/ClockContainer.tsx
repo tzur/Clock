fc330b508182743ba576b4a829f9687a406df60f
Zur Tene <zur@lightricks.com>
2021-03-24 10:42:50 +0200

    Clock: Add server.

--- src/ClockContainer.tsx
+++ src/ClockContainer.tsx
@@ -6,13 +6,19 @@ type ClockState = {
   time: Date
 }
 
+type ClockContainerState = {
+  time: Date,
+  isComputerTime: Boolean
+}
+
 type EmptyProps = {}
 
-class ClockContainer extends Component<EmptyProps, ClockState> {
+class ClockContainer extends Component<EmptyProps, ClockContainerState> {
   constructor(props: EmptyProps) {
     super(props)
     this.state = {
-      time: new Date()
+      time: new Date(),
+      isComputerTime: true
     }
   }
 
@@ -21,11 +27,40 @@ class ClockContainer extends Component<EmptyProps, ClockState> {
   }
 
   private updateClockState() {
-    this.setState({
-      time: new Date()
-    });
+    if (this.state.isComputerTime) {
+      this.setState({
+        time: new Date(),
+      });
+      requestAnimationFrame(this.updateClockState.bind(this))
 
-    requestAnimationFrame(this.updateClockState.bind(this))
+    } else {
+      this.fetchDateFromServer().then(result => {
+          this.setState({
+            time: result
+          })
+          requestAnimationFrame(this.updateClockState.bind(this))
+      })
+    }
+  }
+
+  handleComputerTimeClick() {
+    this.setState({
+      isComputerTime: true
+    }, () => {
+      this.updateClockState()
+    })
+  }
+
+  fetchDateFromServer() {
+    return fetch("http://localhost:8000/clock")
+      .then(response => response.json())
+      .then(data => new Date(data)) as Promise<Date>;
+  }
+
+  handleWorldTimeClick() {
+    this.setState({
+      isComputerTime: false
+    })  
   }
 
   render() {
@@ -33,6 +68,8 @@ class ClockContainer extends Component<EmptyProps, ClockState> {
       <div>
         <DialogClock time={this.state.time}></DialogClock>
         <AnalogClock time={this.state.time}></AnalogClock>
+        <button onClick={this.handleComputerTimeClick.bind(this)}>Computer Time</button>
+        <button onClick={this.handleWorldTimeClick.bind(this)}>World Time</button>
       </div>
     )
   }
