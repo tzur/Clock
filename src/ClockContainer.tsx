ad1ac56ea6df8d04309686f15f74678b28ac6b55
Zur Tene <zur@lightricks.com>
2021-03-30 11:26:30 +0300

    ClockContainer: handle clock offsets and clock stop timing.

--- src/ClockContainer.tsx
+++ src/ClockContainer.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import DialogClock from './DialogClock'
-import AnalogClock from './AnalogClock'
+import AnalogClockContainer, {OffsetNeedle} from './AnalogClockContainer'
 
 type ClockState = {
   time: Date
@@ -8,6 +8,14 @@ type ClockState = {
 
 type ClockContainerState = {
   time: Date,
+  hoursOffset: number,
+  minutesOffset: number,
+  secondsOffset: number,
+  offset: number,
+  changesEndOffset: number,
+  stopwatchTime: Date,
+  clockPauseTime: Date,
+  inOffsetMode: Boolean,
   isComputerTime: Boolean
 }
 
@@ -18,6 +26,14 @@ class ClockContainer extends Component<EmptyProps, ClockContainerState> {
     super(props)
     this.state = {
       time: new Date(),
+      hoursOffset: 0,
+      minutesOffset: 0,
+      secondsOffset: 0,
+      offset: 0,
+      changesEndOffset: 0,
+      stopwatchTime: new Date(),
+      clockPauseTime: new Date(),
+      inOffsetMode: false,
       isComputerTime: true
     }
   }
@@ -26,19 +42,61 @@ class ClockContainer extends Component<EmptyProps, ClockContainerState> {
     this.updateClockState()
   }
 
+  offsetChangesStart() {
+    this.setState({
+      inOffsetMode: true,
+      stopwatchTime: this.currentTime(),
+      clockPauseTime: new Date()
+    })
+  }
+
+  onOffsetChange(offset: number, needle: OffsetNeedle) {
+    switch(needle) {
+      case OffsetNeedle.Hours:
+        this.setState({
+          hoursOffset: offset
+        })
+        return
+      case OffsetNeedle.Minutes:
+        console.log(offset)
+        this.setState({
+          minutesOffset: offset
+        })
+        return
+      case OffsetNeedle.Seconds:
+        this.setState({
+          secondsOffset: offset
+        })
+    }
+  }
+
+  offsetChangesEnd() {
+    const timeStopDelta =  this.state.clockPauseTime.getTime() - new Date().getTime()
+    this.setState({
+      changesEndOffset: this.state.changesEndOffset + timeStopDelta,
+      inOffsetMode: false,
+    })
+  }
+
+  currentTime() {
+    return new Date(new Date().getTime() + this.state.changesEndOffset)
+  }
+
   private updateClockState() {
+    const time = this.state.inOffsetMode ? this.state.stopwatchTime : this.currentTime()
     if (this.state.isComputerTime) {
       this.setState({
-        time: new Date(),
-      });
-      requestAnimationFrame(this.updateClockState.bind(this))
-
+        time: time,
+      }, () => requestAnimationFrame(this.updateClockState.bind(this)));
+  
     } else {
       this.fetchDateFromServer().then(result => {
-          this.setState({
-            time: result
-          })
-          requestAnimationFrame(this.updateClockState.bind(this))
+        if (result === undefined) {
+          return
+        }
+        this.setState({
+          time: result
+        }, () => requestAnimationFrame(this.updateClockState.bind(this)))
       })
     }
   }
@@ -53,8 +111,17 @@ class ClockContainer extends Component<EmptyProps, ClockContainerState> {
 
   fetchDateFromServer() {
     return fetch("http://localhost:8000/clock")
+      .then((response) => {
+        if (!response.ok) {
+          throw Error(response.statusText)
+        }
+        return response
+      })
       .then(response => response.json())
-      .then(data => new Date(data)) as Promise<Date>;
+      .then(data => new Date(data))
+      .catch((error) => {
+        console.log(error)
+      }) as Promise<Date>;
   }
 
   handleWorldTimeClick() {
@@ -63,11 +130,27 @@ class ClockContainer extends Component<EmptyProps, ClockContainerState> {
     })  
   }
 
+  dialogClockTime() {
+    return new Date(this.state.time.getTime() + this.state.hoursOffset + this.state.minutesOffset + this.state.secondsOffset)
+  }
+
+  analogClockContainerState() {
+    return {
+      time: this.state.time,
+      hoursOffset: this.state.hoursOffset,
+      minutesOffset: this.state.minutesOffset,
+      secondsOffset: this.state.secondsOffset
+    }
+  }
+
   render() {
     return (
       <div>
-        <DialogClock time={this.state.time}></DialogClock>
-        <AnalogClock time={this.state.time}></AnalogClock>
+        <DialogClock time={this.dialogClockTime()}></DialogClock>
+        <AnalogClockContainer {...this.analogClockContainerState()}
+          offsetChangeStart={this.offsetChangesStart.bind(this)}
+          offsetChange={this.onOffsetChange.bind(this)} 
+          offsetChangesEnd={this.offsetChangesEnd.bind(this)} ></AnalogClockContainer>
         <button onClick={this.handleComputerTimeClick.bind(this)}>Computer Time</button>
         <button onClick={this.handleWorldTimeClick.bind(this)}>World Time</button>
       </div>
